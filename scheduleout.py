09fc1b47b15f254d4beccd6aab8d8a1dfa1bbf58
luejerry <luejerry@gmail.com>
2017-06-06 12:52:31 -0500

    Add scheduling support for mac and linux using at

--- scheduleout.py
+++ scheduleout.py
@@ -4,6 +4,9 @@ import os
 def schedule(time):
     if os.name == 'nt':
         print('Windows platform detected. Creating Task Scheduler task...')
-        subprocess.call(['powershell.exe', './scheduleout.ps1', time])
-    else:
-        print('TODO: handle Unix')
+        subprocess.run(['powershell.exe', './scheduleout.ps1', time])
+    elif os.name == 'posix':
+        print('POSIX platform detected. Scheduling atjob...')
+        cmd = 'python3 autotimecalc.py out'
+        attime = time + ' today'
+        subprocess.run(['at', attime], input=cmd, encoding='utf-8')
